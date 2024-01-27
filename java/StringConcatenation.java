b07617103cd686a5170f3a6a342d71f2871f11d4
ch9rlene <charlene9876500@gmail.com>
2024-01-27 17:25:37 +0800

    concatenation

---
+++ java/StringConcatenation.java
@@ -0,0 +1,11 @@
+public class StringConcatenation {
+    public static void main(String[] args){
+    int animals = 12;
+    String species = "zebra";
+    /* Use + to make a new String variable called zooDescription. It should hold a String that looks like:
+    DO NOT FORGET SPACES*/
+    String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+    //Print out the variable zooDescription!
+    System.out.println(zooDescription);
+  }       
+}
