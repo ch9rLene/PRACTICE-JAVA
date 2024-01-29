d665115fe35f6cd324580c251264ea1fa63056ea
ch9rlene <charlene9876500@gmail.com>
2024-01-29 23:26:09 +0800

    try

---
+++ java/review.java
@@ -0,0 +1,9 @@
+public class review {
+    public static void main (String[] args){
+        String userName = "Nelrach";
+        int favNum = 17;
+        char initial = 'W';
+
+        System.out.print("Hello, I am " + userName + " and I love " + favNum + ", and it's a " + initial + " for me, if I secured their ticket concert. ");
+    }
+}
